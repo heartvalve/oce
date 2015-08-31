20b45134b66dea051c0311c469243d7444b30e5e
thomas <tpaviot@gmail.com>
2015-08-31 13:27:03 +0200

    kk;)

--- appveyor-scripts/make-oce-msys.sh
+++ appveyor-scripts/make-oce-msys.sh
@@ -31,7 +31,6 @@ cmake -DBUNDLE_BUILD_FREEIMAGE:BOOL=ON \
       -DBUNDLE_SHARED_LIBRARIES:BOOL=ON \
       -DOCE_WIN_BUNDLE_INSTALL_DIR=c:\\oce-win-bundle \
       -G'MSYS Makefiles' ..
-exec 0</dev/null;
 mingw32-make -j4
 mingw32-make install
 #
@@ -45,13 +44,13 @@ cmake -DOCE_VISUALISATION:BOOL=ON \
       -DOCE_WITH_GL2PS:BOOL=OFF \
       -DOCE_WITH_FREEIMAGE:BOOL=OFF \
       -DOCE_TESTING:BOOL=OFF \
-      -DFREETYPE_INCLUDE_DIR_freetype2=C:\oce-win-bundle\include\freetype \
-      -DFREETYPE_INCLUDE_DIR_ft2build=C:\oce-win-bundle\include\freetype \
-      -DFREETYPE_LIBRARY=C:\oce-win-bundle\Win32\lib\freetype.lib \
-      -DFREEIMAGE_INCLUDE_DIR=C:\oce-win-bundle\include\FreeImage \
-      -DFREEIMAGE_LIBRARY=C:\oce-win-bundle\Win32\lib\FreeImage.lib \
-      -DFREEIMAGEPLUS_LIBRARY=C:\oce-win-bundle\Win32\lib\FreeImagePlus.lib \
-      -DOCE_INSTALL_PREFIX=C:\oce-0.17.1-dev \
+      -DFREETYPE_INCLUDE_DIR_freetype2=C:\\oce-win-bundle\\include\\freetype \
+      -DFREETYPE_INCLUDE_DIR_ft2build=C:\\oce-win-bundle/include\\freetype \
+      -DFREETYPE_LIBRARY=c:\\oce-win-bundle\\%Arch%/lib/freetype.lib \
+      -DFREEIMAGE_INCLUDE_DIR=C:\\oce-win-bundle\include\\FreeImage \
+      -DFREEIMAGE_LIBRARY=C:\\oce-win-bundle\\%Arch%\\lib\\FreeImage.lib \
+      -DFREEIMAGEPLUS_LIBRARY=C:\\oce-win-bundle\\%Arch%\\lib\\FreeImagePlus.lib \
+      -DOCE_INSTALL_PREFIX=C:\\oce-0.17.1-dev \
       -G'MSYS Makefiles' ..
 mingw32-make -j4
 make install
