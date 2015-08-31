7d5d618ab0fd2e9423502385ed5b9a643b8694b9
thomas <tpaviot@gmail.com>
2015-08-31 06:42:50 +0200

    avoir

--- appveyor-scripts/make-oce-msvc.bat
+++ appveyor-scripts/make-oce-msvc.bat
@@ -1,3 +1,18 @@
+rem Make OCE win bundle
+
+cd C:\projects\oce-win-bundle
+mkdir cmake-build
+cd cmake-build
+cmake -DBUNDLE_BUILD_FREEIMAGE:BOOL=ON ^
+      -DBUNDLE_BUILD_FREETYPE:BOOL=ON ^
+      -DBUNDLE_BUILD_GL2PS:BOOL=OFF ^
+      -DBUNDLE_SHARED_LIBRARIES:BOOL=ON ^
+      -DOCE_WIN_BUNDLE_INSTALL_DIR=C:\oce-win-bundle ^
+      -G "%generator%" ..
+msbuild /m:4 /verbosity:quiet /clp:ErrorsOnly /p:Configuration=Release oce-win-bundle.sln
+msbuild /m:4 /verbosity:quiet /clp:ErrorsOnly /p:Configuration=Release INSTALL.vcxproj
+
+rem Then make oce
 cd C:\projects\oce
 mkdir cmake-build
 cd cmake-build
--- appveyor-scripts/make-oce-msys.sh
+++ appveyor-scripts/make-oce-msys.sh
@@ -1,6 +1,41 @@
 #!/bin/sh
-echo 'C:\MinGW\ /MinGW' > /etc/fstab
-ls
+set -e
+cd `dirname "$0"`/..
+if [ "$Arch" = "Win32" ]; then
+  echo 'C:\MinGW\ /MinGW' > /etc/fstab
+elif [ "$Arch" = "i686" ]; then
+  f=i686-4.9.2-release-win32-sjlj-rt_v3-rev1.7z
+  if ! [ -e $f ]; then
+    curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.9.2/threads-win32/sjlj/$f
+  fi
+  7z x $f > /dev/null
+  mv mingw32 /MinGW
+else
+  f=x86_64-4.9.2-release-win32-seh-rt_v3-rev1.7z
+  if ! [ -e $f ]; then
+    curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.9.2/threads-win32/seh/$f
+  fi
+  7z x $f > /dev/null
+  mv mingw64 /MinGW
+fi
+g++ -v
+#
+# Build oce dependencies
+#
+cd /c/projects/oce-win-bundle
+mkdir cmake-build
+cd cmake-build
+cmake -DBUNDLE_BUILD_FREEIMAGE:BOOL=ON \
+      -DBUNDLE_BUILD_FREETYPE:BOOL=ON \
+      -DBUNDLE_BUILD_GL2PS:BOOL=OFF \
+      -DBUNDLE_SHARED_LIBRARIES:BOOL=ON \
+      -DOCE_WIN_BUNDLE_INSTALL_DIR=c:\\oce-win-bundle \
+      -G'MSYS Makefiles' ..
+make
+make install
+#
+# Then make oce
+#
 cd /c/projects/oce
 mkdir cmake-build
 cd cmake-build
