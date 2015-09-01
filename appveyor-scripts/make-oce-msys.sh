7b620d7854ed13be5211c7fa79f620e3c3587b6b
tpaviot <tpaviot@gmail.com>
2015-09-01 06:39:08 +0200

    reez

--- appveyor-scripts/make-oce-msys.sh
+++ appveyor-scripts/make-oce-msys.sh
@@ -13,9 +13,7 @@ elif [ "$Arch" = "i686" ]; then
   mv mingw32 /MinGW
 else
   f=x86_64-5.1.0-release-win32-seh-rt_v4-rev0.7z
-  # f=x86_64-4.9.2-release-win32-seh-rt_v3-rev1.7z
   if ! [ -e $f ]; then
-    # curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/4.9.2/threads-win32/seh/$f
     curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.1.0/threads-win32/seh/$f
   fi
   7z x $f > /dev/null
