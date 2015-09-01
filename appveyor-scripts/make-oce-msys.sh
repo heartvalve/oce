4db0db717c1dba443b4751dd9b841b31ecb349cb
tpaviot <tpaviot@gmail.com>
2015-09-01 14:47:07 +0200

    added visu for msys

--- appveyor-scripts/make-oce-msys.sh
+++ appveyor-scripts/make-oce-msys.sh
@@ -40,7 +40,7 @@ mingw32-make install
 cd /c/projects/oce
 mkdir cmake-build
 cd cmake-build
-cmake -DOCE_VISUALISATION:BOOL=OFF \
+cmake -DOCE_VISUALISATION:BOOL=ON \
       -DOCE_DATAEXCHANGE:BOOL=OFF -DOCE_OCAF:BOOL=OFF \
       -DOCE_WITH_GL2PS:BOOL=OFF \
       -DOCE_WITH_FREEIMAGE:BOOL=OFF \
