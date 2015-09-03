bbd7c0dfea943b15b9d8e3ff313683895fbd46f3
tpaviot <tpaviot@gmail.com>
2015-09-03 04:46:02 +0200

    ueert

--- appveyor-scripts/make-oce-msys.sh
+++ appveyor-scripts/make-oce-msys.sh
@@ -4,9 +4,9 @@ cd `dirname "$0"`/..
 if [ "$Arch" = "Win32" ]; then
   echo 'C:\MinGW\ /MinGW' > /etc/fstab
 elif [ "$Arch" = "i686" ]; then
-  f=i686-4.9.2-release-win32-sjlj-rt_v3-rev1.7z
+  f=i686-4.9.3-release-win32-sjlj-rt_v4-rev0.7z
   if ! [ -e $f ]; then
-    curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.9.2/threads-win32/sjlj/$f
+    curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.9.3/threads-win32/sjlj/$f
   fi
   7z x $f > /dev/null
   mv mingw32 /MinGW
