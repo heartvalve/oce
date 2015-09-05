37a0649236336a912b2b3f9ad9d941d9113b4b85
tpaviot <tpaviot@gmail.com>
2015-09-05 21:48:53 +0200

    ert

--- appveyor-scripts/make-oce-msvc.bat
+++ appveyor-scripts/make-oce-msvc.bat
@@ -18,7 +18,7 @@ mkdir cmake-build
 cd cmake-build
 cmake -DOCE_VISUALISATION:BOOL=ON -DOCE_DATAEXCHANGE:BOOL=ON -DOCE_OCAF:BOOL=ON ^
       -DOCE_WITH_GL2PS:BOOL=OFF ^
-      -DOCE_WITH_FREEIMAGE:BOOL=ON ^
+      -DOCE_WITH_FREEIMAGE:BOOL=OFF ^
       -DOCE_TESTING:BOOL=OFF ^
       -DOCE_MULTITHREAD_LIBRARY=NONE ^
       -DFREETYPE_INCLUDE_DIR_freetype2=C:\oce-win-bundle\include\freetype ^
