f30dc00b9a75edc5109dd448fc23d64a4118d1a9
tpaviot <tpaviot@gmail.com>
2015-09-10 06:58:51 +0200

    fixed batch for msvc compilation

--- appveyor-scripts/make-oce-msys.sh
+++ appveyor-scripts/make-oce-msys.sh
@@ -4,17 +4,17 @@ cd `dirname "$0"`/..
 echo "$Arch"
 if [ "$Arch" = "Win32" ]; then
   echo 'C:\MinGW\ /MinGW' > /etc/fstab
-elif [ "$Arch" = "i686" ]; then
-  f=i686-4.9.3-release-win32-sjlj-rt_v4-rev0.7z
+elif [ "$Arch" = "i686" ]; then 
+  f=i686-4.9.3-release-posix-dwarf-rt_v4-rev1.7z
   if ! [ -e $f ]; then
-    curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.9.3/threads-win32/sjlj/$f
+    curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.9.3/threads-posix/dwarf/$f
   fi
   7z x $f > /dev/null
   mv mingw32 /MinGW
 else
-  f=x86_64-5.1.0-release-win32-seh-rt_v4-rev0.7z
+  f=x86_64-5.2.0-release-posix-seh-rt_v4-rev0.7z
   if ! [ -e $f ]; then
-    curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.1.0/threads-win32/seh/$f
+    curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.2.0/threads-posix/seh/$f
   fi
   7z x $f > /dev/null
   mv mingw64 /MinGW
@@ -26,8 +26,8 @@ mkdir cmake-build
 cd cmake-build
 cmake -DOCE_VISUALISATION:BOOL=ON \
       -DOCE_DATAEXCHANGE:BOOL=OFF -DOCE_OCAF:BOOL=OFF \
-      -DOCE_WITH_GL2PS:BOOL=OFF \
-      -DOCE_WITH_FREEIMAGE:BOOL=ON \
+      -DOCE_WITH_GL2PS:BOOL=ON \
+      -DOCE_WITH_FREEIMAGE:BOOL=OFF \
       -DOCE_TESTING:BOOL=ON \
       -DOCE_COPY_HEADERS_BUILD:BOOL=ON \
       -DOCE_INSTALL_PREFIX=C:\\oce-0.17.1-dev \
