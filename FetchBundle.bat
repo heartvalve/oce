52dab9d4b8fcabd94f3a379a9c84d80a2fce13a6
tpaviot <tpaviot@gmail.com>
2015-09-10 06:10:37 +0200

    Fixed FetchBundle.bat

--- FetchBundle.bat
+++ FetchBundle.bat
@@ -1,11 +1,8 @@
 @echo off
 if "%1"=="/help" goto help
 if "%1"=="--help" goto help
-git clone http://github.com/QbProg/oce-win-bundle
+git clone --branch=qb/new-bundle https://github.com/QbProg/oce-win-bundle.git
 if "%1"=="" goto end
-cd oce-win-bundle
-git checkout oce-win-bundle-%1
-cd ..
 goto end
 :help
 echo "Usage : FetchBundle.bat <version>"
