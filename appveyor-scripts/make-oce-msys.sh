d6fc3a84feeb7c848d04cc8224ad04ebaee7ad35
tpaviot <tpaviot@gmail.com>
2015-09-12 07:31:45 +0200

    fixed strings

--- appveyor-scripts/make-oce-msys.sh
+++ appveyor-scripts/make-oce-msys.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 set -e
 cd `dirname "$0"`/..
-echo "$Arch"
-if [ "$Arch" = "Win32" ]; then
+if [ "$ARCH" = Win32 ]; then
   echo 'C:\MinGW\ /MinGW' > /etc/fstab
-elif [ "$Arch" = "i686" ]; then 
+elif [ "$ARCH" = i686 ]; then 
   f=i686-4.9.3-release-posix-dwarf-rt_v4-rev1.7z
   if ! [ -e $f ]; then
+    echo "Downloading $f"
     curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win32/Personal%20Builds/mingw-builds/4.9.3/threads-posix/dwarf/$f
   fi
   7z x $f > /dev/null
+  echo "Extracting $f"
   mv mingw32 /MinGW
 else
   f=x86_64-5.2.0-release-posix-seh-rt_v4-rev0.7z
   if ! [ -e $f ]; then
+    echo "Downloading $f"
     curl -LsSO http://sourceforge.net/projects/mingw-w64/files/Toolchains%20targetting%20Win64/Personal%20Builds/mingw-builds/5.2.0/threads-posix/seh/$f
   fi
+  echo "Extracting $f"
   7z x $f > /dev/null
   mv mingw64 /MinGW
 fi
@@ -37,5 +40,5 @@ mingw32-make install > /dev/null
 #
 # Finally run tests
 #
-export PATH=$PATH:/c/MinGW/bin:/c/oce-0.17.1-dev/$Arch/bin:/c/MinGW/bin:
+export PATH=$PATH:/c/MinGW/bin:/c/oce-0.17.1-dev/$ARCH/bin:/c/MinGW/bin:
 mingw32-make test
